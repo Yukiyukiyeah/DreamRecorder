5a9a07d3d96e8a1f2cfd2a700ef4295dc855300d
yukiyukiyeah <yukitang0703@gmail.com>
2021-08-18 23:31:28 +0800

    add google oauth:

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-25 18:53:08
- * @LastEditTime: 2021-07-25 20:13:59
+ * @LastEditTime: 2021-08-18 23:22:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /DreamRecorder/server.js
@@ -11,10 +11,29 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/key')
+
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken)
+  })
+)
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
 
 app.use(cors())
 app.use(express.json())
@@ -32,6 +51,7 @@ const usersRouter = require('./routes/users')
 app.use('/dreams', dreamsRouter)
 app.use('/users', usersRouter)
 
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 })
